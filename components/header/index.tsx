c0cb3c99ad7a3c19768c75b8282d4d521c0290c2
Mikael <mikaelmuniz2001@gmail.com>
2022-12-15 23:38:49 -0300

    feat: setup responsive to header title and logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -4,11 +4,11 @@ const Header = () => {
         <header>
             <div className="bg-cover bg-header h-screen grid grid-cols-6 gap-4 content-center ...">
                 <div className="col-start-2 col-span-3">
-                    <span className="text-6xl font-league font-semibold whitespace-nowrap text-standard w-50">UNIÃO <br /> JUVENTUDE E <br /> LIBERDADE</span>
-                    <p className="border-b w-56">Faremos história!</p>
+                    <span className="md:text-7xl sm:text-5xl text-2xl font-league font-semibold text-standard">UNIÃO <br /> JUVENTUDE E <br /> LIBERDADE</span>
+                    <p className="border-b border-[#FFC700] sm:w-56 w-28">Faremos história!</p>
                 </div>
                 <div className="col-start-5 col-end-8">
-                    <Image src="/img/logo.svg" alt="Logo" width={200} height={30} />
+                    <Image src="/img/logo.svg" alt="Logo" width={200} height={100} />
                 </div>
             </div>
         </header>
