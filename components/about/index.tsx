71ac4d7bcb34e8b7dc3b7e8fd4a5596f6cfcb017
Mikael <mikaelmuniz2001@gmail.com>
2022-12-24 00:39:54 -0300

    creating 'about' section

---
+++ components/about/index.tsx
@@ -0,0 +1,42 @@
+
+
+const About = () => {
+    return <>
+        <div className="text-center">
+            <span className="text-5xl text-standard items-center">
+                <b>
+                    QUEM SOMOS
+                </b>
+            </span>
+            <div className="grid grid-cols-12 text-left">
+                <div className="col-start-1 col-end-6">
+
+                </div>
+                <div className="col-start-7 col-end-13 bg-red">
+                    <p className="mt-2">
+                        A UJL, União, Juventude e Liberdade,
+                        é uma união de jovens dispostos a discutir os problemas enfrentados por estudantes secundaristas e universitários brasileiros,
+                        pautando sempre a defesa das liberdades individuais.
+                    </p>
+                    <p className="mt-2">
+                        Surgida de dentro da ONG Students For Liberty Brazil, em 4 de julho de 2020,
+                        a UJL foi criada para participar do LibertyLab e acabou se tornando a vencedora do Hackaton Liberal no começo de 2021.
+                        Posteriormente, fomos agraciados com os três prêmios que compõem o SFLB Awards e disputamos o prêmio internacional de grupo do ano pelo SFL Inc.
+                    </p>
+                    <p className="mt-2">
+                        Em um ano, sem qualquer tipo de orçamento,
+                        a UJL veio a se tornar o maior grupo estudantil liberal do Brasil, contando agora com quase três mil membros e mais de 200 lideranças ativas em todos os estados da federação e no Distrito Federal. Ao longo de dois anos, foram mais de 20 mil reais arrecadados para diversos projetos sociais,
+                        além de parcerias fechadas com diversas instituições em mais de 5 países diferentes.
+                    </p>
+                    <p className="mt-2">
+                        Estamos apenas começando, 
+                        se você quer ser um agente de mudança e defender a liberdade na sua universidade, 
+                        vem conosco fazer história
+                    </p>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/header'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer/index'
+import About from '../components/about'
 
 export default function Home() {
   return <>
     <NavBar />
     <Header />
+    <About />
     <Footer />
   </>
 };
