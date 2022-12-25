8ae8b36153fef615d8f6aae911b8578d0a1f8e63
Mikael <mikaelmuniz2001@gmail.com>
2022-12-25 11:42:02 -0300

    creating 'valores' section

---
+++ components/sections/valores/index.tsx
@@ -0,0 +1,17 @@
+const Valores = () => {
+    return <>
+        <div className="grid place-items-center bg-standard h-56">
+            <div className="text-center w-2/6 h-32 bg-black rounded mt-4">
+                <h3 className="text-4xl text-standard place-items-center mt-4">
+                    <strong className="mt-9">
+                        NO QUE
+                        <br />
+                        ACREDITAMOS
+                    </strong>
+                </h3>
+            </div>
+        </div>
+    </>;
+}
+
+export default Valores; 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import Header from '../components/header'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer/index'
 import About from '../components/about'
+import Valores from '../components/sections/valores'
 
 export default function Home() {
   return <>
     <NavBar />
     <Header />
+    <Valores />
     <About />
     <Footer />
   </>
